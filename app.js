e804bc3eb9e3e42c369df13511c38db95fac5bd9
Alan Tran <contactalantran@gmail.com>
2020-11-08 14:54:18 -0500

    add name label and finish project

--- app.js
+++ app.js
@@ -2,17 +2,28 @@ const player1 = {
     addPoint: document.querySelector("#upPoint1"),
     currentScore: 0,
     displayScore: document.querySelector("#playerOneScore"),
-    roundsWon: 0
+    roundsWon: 0,
+    name: prompt("What is Player One's name?"),
+    nameLabel: document.querySelector("#playerOneName")
 }
 
 const player2 = {
     addPoint: document.querySelector("#upPoint2"),
     currentScore: 0,
     displayScore: document.querySelector("#playerTwoScore"),
-    roundsWon: 0
+    roundsWon: 0,
+    name: prompt("What is Player Two's name?"),
+    nameLabel: document.querySelector("#playerTwoName")
 }
+
+player1.addPoint.textContent = `+1 for ${player1.name}`;
+player1.nameLabel.textContent = player1.name;
+player2.addPoint.textContent = `+1 for ${player2.name}`;
+player2.nameLabel.textContent = player2.name;
+
 let isGameOver = false;
 let playTo = 5;
+let subtitle = document.querySelector(".subtitle")
 const resetButton = document.querySelector("#reset")
 
 function pointIncrease(player, opponent) {
@@ -22,6 +33,7 @@ function pointIncrease(player, opponent) {
         if(player.currentScore == playTo) {
             player.displayScore.classList.toggle("won");
             opponent.displayScore.classList.toggle("lost");
+            subtitle.textContent = `${player.name} won!`;
             isGameOver = true;
         }
     }
@@ -32,6 +44,7 @@ function resetGame() {
         player.currentScore = 0;
         player.displayScore.textContent = player.currentScore;
         player.displayScore.classList.remove("won", "lost");
+        subtitle.textContent = "Okay, let's try this again";
         isGameOver = false;
     }
 }
